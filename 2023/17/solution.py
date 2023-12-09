e48c42b2f2b21b5785b1da23404752595f6e4a99
Matthias Wetter <11140006+m2u-84@users.noreply.github.com>
2023-12-09 20:34:08 +0100

    Implement solutions for day 2023-12-09 (riddles 17 + 18)

---
+++ 2023/17/solution.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+sum = 0
+
+def calculate_transition_sequence(original_sequence: list[int]) -> list[int]:
+	if len(original_sequence) < 2:
+		raise ValueError('Sequence too short.')
+
+	transition_sequence: list[int] = []
+
+	for index, value in enumerate(original_sequence):
+		if index == 0:
+			continue
+
+		transition_sequence.append(value - original_sequence[index - 1])
+
+	return transition_sequence
+
+with open('input.txt', 'r') as input_file:
+	for line in input_file:
+		sequence = list(map(lambda value: int(value), line.rstrip().split()))
+		next_number = 0
+
+		while any(sequence):
+			next_number += sequence[-1]
+
+			sequence = calculate_transition_sequence(
+				original_sequence = sequence
+			)
+
+		sum += next_number
+
+print(sum)
